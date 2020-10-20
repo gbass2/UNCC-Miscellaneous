d0312d96131401eee7bc254c3b7ef35be28e61ec
gbass2 <57140006+gbass2@users.noreply.github.com>
2020-10-20 15:39:29 -0400

    Update countInv.cpp

--- C++/Data Stuctures/Merge Sort Inversions Counting/countInv.cpp
+++ C++/Data Stuctures/Merge Sort Inversions Counting/countInv.cpp
@@ -2,13 +2,15 @@
 // Based off of mergesort
 #include <vector>
 #include <algorithm> // For copy
+#include <iostream>
 
 using namespace std;
 
 
-int mergeInv(vector<int>& nums, vector<int>& left, vector<int>& right) {
+int mergeInv(vector<int>& nums, vector<int> &left, vector<int> &right) {
 // You will need this helper function that calculates the inversion while merging
 // Your code here
+    int count = 0;
     int i = 0, j = 0, k = 0;
 	while ((j < left.size()) && (k < right.size())) {
 		if (left[j] <= right[k]) {
@@ -17,10 +19,12 @@ int mergeInv(vector<int>& nums, vector<int>& left, vector<int>& right) {
 			j++;
 		} else {
 			nums[i] = right[k];
-			i++;
+
 			k++;
-		}
+            count += left.size() - j;
+        }
 	}
+
 	while(j < left.size())  { // Remaining elements of left
 		nums[i] = left[j];
 		i++;
@@ -31,19 +35,27 @@ int mergeInv(vector<int>& nums, vector<int>& left, vector<int>& right) {
 		i++;
 		k++;
 	}
+
+    return count;
 }
 
 int countInv(vector<int>&nums) {
 // Your code here
+    int count = 0;
     if (nums.size() > 1) {
         int mid = nums.size()/2;
         vector<int> left(mid);
         vector<int> right(nums.size()-mid);
         copy(nums.begin(), nums.begin() + mid, left.begin());
         copy(nums.begin() + mid, nums.end(), right.begin());
-        countInv(left);
-        countInv(right);
-        mergeInv(nums, left, right);
 
-    }
+        count += countInv(left);
+        count += countInv(right);
+
+        count += mergeInv(nums, left, right);
+        return count;
+
+    // Base case if size = 1
+    } else
+        return 0;
 }
